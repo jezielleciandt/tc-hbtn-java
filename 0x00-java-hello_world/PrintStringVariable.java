65b8e6a2a0ceace6ef8c2b1a12ee2a3fc06178aa
root <root@lnb036055cps.ciandt.global>
2022-06-13 17:38:29 -0300

    Task 3.Print String Variable

--- 0x00-java-hello_world/PrintStringVariable.java
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -2,6 +2,6 @@ public class PrintStringVariable {
     public static void main(String args[]){
         String texto = "Holberton School";
         System.out.print(texto + "\n" + texto + "\n" + texto.substring(0,9) + "\n");
-        
     }
+
 }
