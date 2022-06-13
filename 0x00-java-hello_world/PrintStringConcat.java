9d12d59b9ad2f365e18f996705728ac5f77ce445
root <root@lnb036055cps.ciandt.global>
2022-06-13 18:32:59 -0300

    Task 4.Concatenação

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -2,7 +2,7 @@ public class PrintStringConcat {
     public static void main(String args[]){
         String texto1 = "Holberton";
         String texto2 = "School";
-        String resultado = "Bem vindo a " + texto1 + " " + texto2;
+        String resultado = "Bem vindo a " + texto1 + " " + texto2 + "!";
         System.out.println(resultado);
-    }
-}
+        System.out.println();
+    }}
