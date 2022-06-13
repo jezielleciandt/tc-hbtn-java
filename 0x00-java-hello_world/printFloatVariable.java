64b3d17c9c680a5e02817898821e0a8dab28c32d
root <root@lnb036055cps.ciandt.global>
2022-06-13 16:47:34 -0300

    Task 2.Print Float Variable

---
+++ 0x00-java-hello_world/printFloatVariable.java
@@ -0,0 +1,6 @@
+public class printFloatVariable {
+    public static void main(String args[]){
+        float pi = (float) 3.14159;
+        System.out.println(String.format("Valor de pi: %.3f.", pi));
+    }
+}
