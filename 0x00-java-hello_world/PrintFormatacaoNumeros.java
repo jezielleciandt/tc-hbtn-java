2f0e32602ec643e8f91096371e72738fb8066d68
root <root@lnb036055cps.ciandt.global>
2022-06-13 21:27:36 -0300

    Task 5.Formatando números

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -1,16 +1,11 @@
-import java.text.DecimalFormat;
 import java.text.NumberFormat;
-import java.util.Locale;
-
 public class PrintFormatacaoNumeros {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
-        Locale localeBR = new Locale("pt ","BR");
-        NumberFormat moeda = NumberFormat.getCurrencyInstance(localeBR);
-        NumberFormat formatter = new DecimalFormat("#0.00");
-        System.out.println("Valor: " + moeda.format(valor));
-        System.out.print("Taxa: " + formatter.format(taxa).concat("%"));
-        System.err.println();
+
+        String valorMonetario = NumberFormat.getCurrencyInstance().format(valor).replace("$", "R$ ");
+
+        System.out.printf("Valor: %s\nTaxa: %.2f%%\n", valorMonetario, taxa);
     }
 }
