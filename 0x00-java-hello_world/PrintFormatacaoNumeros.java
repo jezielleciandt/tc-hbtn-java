99ec6790ff28efc5b3f0c19e087c50effb9d6f49
root <root@lnb036055cps.ciandt.global>
2022-06-13 21:37:56 -0300

    Task 5.Formatando números outra solução

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -1,11 +1,22 @@
 import java.text.NumberFormat;
+import java.util.Locale;
+
 public class PrintFormatacaoNumeros {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
 
         String valorMonetario = NumberFormat.getCurrencyInstance().format(valor).replace("$", "R$ ");
 
         System.out.printf("Valor: %s\nTaxa: %.2f%%\n", valorMonetario, taxa);
+
+        //Outra solução:
+        Locale localeBR = new Locale("pt","BR");
+        NumberFormat percentual = NumberFormat.getPercentInstance(localeBR);
+        NumberFormat moeda = NumberFormat.getCurrencyInstance(localeBR);
+
+        System.out.println("Valor: " + moeda.format(valor) + "\n" + "Taxa: " + percentual.format(taxa));
+
     }
+
 }
