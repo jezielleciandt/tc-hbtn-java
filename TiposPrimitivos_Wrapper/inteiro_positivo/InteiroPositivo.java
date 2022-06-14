e532ee2e243a9d991e172db5db1683684ce81a16
root <root@lnb036055cps.ciandt.global>
2022-06-14 15:39:32 -0300

    Task 0.Inteiro da trila Tipos primitivos e wrappers

---
+++ TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -0,0 +1,48 @@
+import static java.lang.Integer.parseInt;
+
+public class InteiroPositivo {
+    private Integer valor;
+
+    public InteiroPositivo(int valor) throws Exception {
+        naoEhPositivo(valor);
+        this.valor = valor;
+    }
+
+    public InteiroPositivo(String valor) throws Exception {
+        naoEhPositivo(Integer.parseInt(valor));
+        this.valor = Integer.parseInt(valor);
+
+    }
+    public Exception naoEhPositivo(Integer valor) throws Exception{
+        if(valor<0){
+            throw new IllegalAccessException("Valor nao eh um valor inteiro positivo");
+        }
+        return null;
+    }
+    public InteiroPositivo() {
+    }
+
+    public Integer getValor() {
+        return valor;
+    }
+
+    public void setValor(Integer valor) throws Exception {
+        naoEhPositivo(valor);
+        this.valor = valor;
+    }
+
+    public boolean ehPrimo() {
+        if (this.valor == 1) {
+            return false;
+        }
+
+        for (int i = 2; i < this.valor; i++) {
+            if (this.valor % i == 0)
+                return false;
+        }
+
+        return true;
+
+    }
+
+}
