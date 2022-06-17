f53fe54cf312d863cda980e50e51e04efb5c985f
root <root@lnb036055cps.ciandt.global>
2022-06-17 12:19:55 -0300

    Task 4.Consistência

---
+++ 0x04/consistencia/PersonagemGame.java
@@ -0,0 +1,62 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+
+    private String status;
+
+    public PersonagemGame(int saudeAtualInicial, String nomeInicial){
+        setSaudeAtual(saudeAtualInicial);
+        setNome(nomeInicial);
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        atualizaStatus(this.saudeAtual);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome != null && !nome.isBlank()){
+            this.nome = nome;
+        }
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        if("vivo".equals(status) || "morto".equals(status)){
+            this.status = status;
+        }
+    }
+
+    public void tomarDano(int quantidadeDeDano){
+        if(quantidadeDeDano>=saudeAtual){
+            setSaudeAtual(0);
+        }else{
+            setSaudeAtual(saudeAtual-quantidadeDeDano);
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura){
+        setSaudeAtual(saudeAtual+quantidadeDeCura);
+        if(saudeAtual>100){
+            setSaudeAtual(100);
+        }
+    }
+    private void atualizaStatus(int saudeAtual){
+        if(saudeAtual>0){
+            setStatus("vivo");
+        }else{
+            setStatus("morto");
+        }
+    }
+}
