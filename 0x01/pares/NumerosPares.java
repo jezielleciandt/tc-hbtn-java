1b44d5df4462c022f8901524eb903ea367601f92
root <root@lnb036055cps.ciandt.global>
2022-06-21 15:13:10 -0300

    Task 1. Numeros pares de 0 a 99

--- 0x01/pares/NumerosPares.java
+++ 0x01/pares/NumerosPares.java
@@ -1,14 +1,11 @@
 public class NumerosPares {
 
     public static void main(String[] args) {
-        int num = -2;
-        for(int i = 0; i<=48; i++){
-            num += 2;
+        int num;
+
+        for (num = 0; num < 97; num += 2) {
             System.out.print(num + ", ");
-            if(i == 48){
-                num+= 2;
-                System.out.printf("%d\n",num);
-            }
         }
+        System.out.printf("%d\n", num);
     }
 }
