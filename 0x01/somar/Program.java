49e9e5812d1c3116b864b8e9d87b6c10f0f5c1b8
root <root@lnb036055cps.ciandt.global>
2022-06-21 15:53:52 -0300

    Task 3. Soma

--- 0x01/somar/Program.java
+++ 0x01/somar/Program.java
@@ -1,4 +1,4 @@
-public class Probram {
+public class Program {
     public static void main(String[] args) {
         System.out.printf("%d\n", Numero.somar(24, 32));
         System.out.printf("%d\n", Numero.somar(5, 0));
