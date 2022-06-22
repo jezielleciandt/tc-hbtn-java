e5bd75a5b17455aa89e7d3b8c840377bcbf27c33
root <root@lnb036055cps.ciandt.global>
2022-06-22 15:02:20 -0300

    Task 0. Divisão inteira

---
+++ 0x02/divisao/Numero.java
@@ -0,0 +1,14 @@
+public class Numero {
+    public static void dividir(int a, int b){
+        int result = 0;
+        try {
+            result = a/b;
+        }
+        catch (ArithmeticException e){
+            System.out.println("Nao eh possivel dividir por zero");
+        }
+        finally {
+            System.out.println(a + " / " + b + " = " + result);
+        }
+    }
+}
