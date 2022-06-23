029610865991873264670c2787f2602bc55dcf7a
root <root@lnb036055cps.ciandt.global>
2022-06-23 15:09:18 -0300

    Task 5.Classe Abstrata

---
+++ Heranca/abstrata/Quadrado.java
@@ -0,0 +1,23 @@
+public class Quadrado extends Retangulo{
+
+    private double lado;
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        if(lado < 0){
+            throw  new IllegalArgumentException("Lado deve ser maior ou igual a 0");
+        }
+        this.lado = lado;
+    }
+    @Override
+    public double area(){
+        return Math.pow(lado, 2);
+    }
+    @Override
+    public String toString(){
+        return String.format("[Quadrado] %.2f", lado);
+    }
+}
