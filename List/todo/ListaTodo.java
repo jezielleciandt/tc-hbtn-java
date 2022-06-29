ac13f824c7aae862a60af97074359dca952c0010
root <root@lnb036055cps.ciandt.global>
2022-06-29 00:12:03 -0300

    Task 2. Lista TODO

--- List/todo/ListaTodo.java
+++ List/todo/ListaTodo.java
@@ -5,7 +5,7 @@ public class ListaTodo {
     private List<Tarefa> tarefas;
 
     public ListaTodo() {
-        this.tarefas = new ArrayList<>();
+        this.tarefas = new ArrayList<Tarefa>();
     }
 
     public void adicionarTarefa(Tarefa novaTarefa) {
