a892ed5013de4efd8518ddb0784a8ae0fa5afc95
root <root@lnb036055cps.ciandt.global>
2022-07-06 18:51:58 -0300

    Task 1.Armazéns específicos

---
+++ Generics_Exercises/armazem_especifico/ArmazemDeEletronico.java
@@ -0,0 +1,7 @@
+public class ArmazemDeEletronico extends  Armazem<Eletronico>{
+
+    @Override
+    public void adicionarAoInventario(String nome, Eletronico valor) {
+        super.adicionarAoInventario(nome, valor);
+    }
+}
